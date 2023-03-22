86a1038a4ed95cf57b037fd2448478cd258918b1
Felix <felixadamietz@gmx.de>
2023-03-22 16:38:25 +0100

    First push!

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BackupCopyFiles
+{
+    class Program
+    {
+        static Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+        static Tools tools = new Tools();
+        static string source = default, 
+            target = default, 
+            backupFolder = default, 
+            projectname = default;
+
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                GetInformations();
+                return;
+            }
+            else if (!args.Contains("-pathGet") || !args.Contains("-pathSet"))
+            {
+                Console.WriteLine("You cannot continue. Please set any informations in commandline args!");
+                GetInformations();
+                return;
+            }
+            else if(args.Length % 2 == 1) //0 == gerade anzahl der elemente des arrays ---- 1 == ungerade anzahl der elemente des arrays
+            {
+                Console.WriteLine("CommanlineArgs not valid. Please check and try again!");
+                GetInformations();
+                return;
+            }
+            
+            Console.WriteLine("Initializing Backup Data");
+            //get commandline args
+            parameters = tools.GetCommandLineArgs(parameters);
+
+            //get parameters values
+            parameters.TryGetValue("-pathGet", out source);
+            parameters.TryGetValue("-pathSet", out target);
+            parameters.TryGetValue("-backupfolder", out backupFolder);
+            parameters.TryGetValue("-projectname", out projectname);
+            try
+            {
+                tools.CopyFilesRecursively(source, target + "\\" + backupFolder + "\\" + projectname);
+                Console.WriteLine("press 'return' to continue...");    
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: parsing cmd args");
+#if DEBUG
+                Console.WriteLine(ex.ToString());
+#endif
+            }
+            Console.ReadLine();
+        }
+
+        private static void GetInformations()
+        {
+            Console.WriteLine("You have to add some commandline args\n");
+            Console.WriteLine("-pathGet \"PATH to source DIR\"");
+            Console.WriteLine("-pathSet \"PATH to destination DIR\"");
+            Console.WriteLine("optional:");
+            Console.WriteLine("-backupfolder \"name your main backup folder\"");
+            Console.WriteLine("-projectname \"name your current project\"");
+        }
+
+    }
+}
---
+++ Tools.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+
+namespace BackupCopyFiles
+{
+    class Tools
+    {
+        public Dictionary<string,string> GetCommandLineArgs(Dictionary<string, string> parameters)
+        {
+            Console.WriteLine("Get CommandLine Args\n");
+            string[] CommandLineArgs = Environment.GetCommandLineArgs();
+            parameters = new Dictionary<string, string>
+            {
+                { "-pathGet", "" },
+                { "-pathSet", "" },
+                { "-backupfolder", "" },
+                { "-projectname", "" }
+            };
+
+            if (CommandLineArgs.Length > 1)
+            {
+                for (int i = 0; i < CommandLineArgs.Length; i++)
+                {
+                    if (CommandLineArgs[i].StartsWith("-pathGet"))
+                        parameters["-pathGet"] += CommandLineArgs[i + 1];
+                    if (CommandLineArgs[i].StartsWith("-pathSet"))
+                        parameters["-pathSet"] += CommandLineArgs[i + 1];
+                    if (CommandLineArgs[i].StartsWith("-backupfolder"))
+                        parameters["-backupfolder"] += CommandLineArgs[i + 1];
+                    if (CommandLineArgs[i].StartsWith("-projectname"))
+                        parameters["-projectname"] += GetTimestamp(DateTime.Now) + "_" + CommandLineArgs[i + 1];
+                }
+                if (parameters["-projectname"] == "")
+                    parameters["-projectname"] += GetTimestamp(DateTime.Now) + "_DEMOPROJECT";
+            }
+            return parameters;
+        }
+
+        public void CopyFilesRecursively(string sourcePath, string targetPath)
+        {
+            Console.WriteLine("Copy files - starting...\n");
+            //Now Create all of the directories
+            foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+            {
+                Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+            }
+
+            int index = 0,
+                percentage = 0,
+                length = Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories).Length;
+            long dirSize = GetDirectorySize(sourcePath);
+            long copiedSize = 0L;
+            FileInfo fi = null;
+            
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+            {
+                ++index;
+                percentage = 100 * index / length;
+                fi = new FileInfo(newPath);
+                copiedSize += fi.Length;
+                File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
+                Console.Write("\r{0}/{1}   {2}%   {3}MB copied", index, length, percentage, copiedSize / 1024 / 1024);
+            }
+            Console.WriteLine("\n\nCopy files - {0}MB - done!", dirSize / 1024 / 1024);
+        }
+
+        private static long GetDirectorySize(string folderPath)
+        {
+            DirectoryInfo di = new DirectoryInfo(folderPath);
+            return di.EnumerateFiles("*", SearchOption.AllDirectories).Sum(fi => fi.Length);
+        }
+
+        public static String GetTimestamp(DateTime value)
+        {
+            return value.ToString("yyyyMMddHHmmss");
+        }
+    }
+}
